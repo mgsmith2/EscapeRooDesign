a579c45084e4bd12d808778ae04568030cdd5a91
mgsmith2 <97557388+mgsmith2@users.noreply.github.com>
2022-04-26 17:08:26 -0400

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,10 @@
+function colorAnswer() {
+   var password = document.getElementById("passwordBox2");
+  /*add .toUpperCase() String function so that even if the user types in the password in lower-case, it will check against uppercase password text*/
+   var passwordText = password.value.toUpperCase();
+   if(passwordText == "GOLD KEY") {
+    return true;
+   }
+   alert("That is incorrect, try again!");
+   return false;
+   }
