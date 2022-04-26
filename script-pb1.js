a8b7b77027df73d08c4576fdde35466f445e83b1
mgsmith2 <97557388+mgsmith2@users.noreply.github.com>
2022-04-26 16:41:19 -0400

    Update script-pb1.js

--- script-pb1.js
+++ script-pb1.js
@@ -3,7 +3,7 @@ function colorAnswer() {
   /*add .toUpperCase() String function so that even if the user types in the password in lower-case, it will check against uppercase password text*/
    var passwordText = password.value.toUpperCase();
    if(passwordText == "THE CODE IS IN THE NEWS REPORT") {
-     window.location.href = 'https://codepen.io/mgsmith2/pen/wvpMpgd';
+     window.location.href = 'https://mgsmith2.github.io/EscapeRooDesign/puzzle-2.html';
      return true;
    }
    alert("That is incorrect, try again!");
